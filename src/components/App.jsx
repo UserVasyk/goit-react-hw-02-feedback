3fbe4b72d4e78e68ec7e06e9849ba291509a869b
UserVasyk <106909139+UserVasyk@users.noreply.github.com>
2023-01-13 10:34:17 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+// import Statistics from './Statistics/Statistics';
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  countTotalFeedback() {
+    console.log('this.state: ', this.state);
+  }
+  countPositiveFeedbackPercentage() {}
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <h1>Please Leave Feedback</h1>
+        <div>
+          <button
+            type="button"
+            onClick={event => {
+              console.log('click');
+            }}
+          >
+            Good
+          </button>
+          <button type="button">Neutral</button>
+          <button type="button">Bad</button>
+        </div>
+        <div>
+          <h2>Statistics</h2>
+          <ul>
+            <li>Good: {good}</li>
+            <li>Neutral:{neutral}</li>
+            <li>Bad:{bad}</li>
+            <li>Total{this.countTotalFeedback()}</li>
+            <li>Posiitive Feedback</li>
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
